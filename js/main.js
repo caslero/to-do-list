02406c23d5c0b796a929156873da5a0343d0a449
carlo <carlosjperazab@gmail.com>
2023-06-24 12:55:31 -0400

    Le agregamos estilos a los botones de cambiar de pagina y donde muestra la pagina actual junto con el numero de paginas

--- js/main.js
+++ js/main.js
@@ -319,322 +319,142 @@ function eliminarMarcados() {
 
 
 function desmarcado(){
-
     let cantidadPaginas = todos.length / limite;
     let x = Math.ceil(cantidadPaginas);
-    //console.log('Pagina: ' + pagina + ' / ' + x + ' en primeros datos');
-    document.getElementById('numeroDePaginas').innerHTML = 'Pagina: ' + pagina + ' / ' + x + ' en primeros datos';
+    
+    document.getElementById('numeroDePaginas').innerHTML = 'Pagina: ' + pagina + ' / ' + x;
     if (pagina <= 1) {
         anterior.classList.add('hidden');
         siguiente.classList.add('hidden');
     }
-     pagina = pagina - 1;
-     if (pagina <= x) {
+
+    pagina = pagina - 1;
+    if (pagina <= x) {
         anterior.classList.add('hidden');
         
-        const total_no_of_items = todos.length;
-        const items_per_page = 3;
-            //let no_of_pages = total_no_of_items / items_per_page;
-            //let page_number = 1;         
-        const items_to_skip = (pagina - 1) * items_per_page;
-        let items = todos.slice(items_to_skip, items_per_page + items_to_skip);
+        const total_no_of_items = todos.length;       
+        const items_to_skip = (pagina - 1) * limite;
+        let items = todos.slice(items_to_skip, limite + items_to_skip);
         renderTodoList();
         items.map((todo) => {                        
             renderTodo(todo);
-        });
-        
-     }
-
-     
-   
-   
- 
+        });        
+     } 
 }
+
 
 function primerosDatos() {
     
     let cantidadPaginas = todos.length / limite;
     let x = Math.ceil(cantidadPaginas);
-    console.log('Pagina: ' + pagina + ' / ' + x + ' en primeros datos');
-    document.getElementById('numeroDePaginas').innerHTML = 'Pagina: ' + pagina + ' / ' + x + ' en primeros datos';
+    document.getElementById('numeroDePaginas').innerHTML = 'Pagina: ' + pagina + ' / ' + x;
     
     if (pagina <= 1) {
         siguiente.classList.add('hidden');
         anterior.classList.add('hidden');
         const total_no_of_items = todos.length;
-        const items_per_page = 3;
-        //let no_of_pages = total_no_of_items / items_per_page;
-        //let page_number = 1;         
-    const items_to_skip = (pagina - 1) * items_per_page;
-    let items = todos.slice(items_to_skip, items_per_page + items_to_skip);
-    renderTodoList();
-    items.map((todo) => {                        
-        renderTodo(todo);
-    });
+        const items_to_skip = (pagina - 1) * limite;
+        let items = todos.slice(items_to_skip, limite + items_to_skip);
+        renderTodoList();
+        items.map((todo) => {                        
+            renderTodo(todo);
+        });
     } else if (pagina <= x) {
-    const total_no_of_items = todos.length;
-    const items_per_page = 3;
-        //let no_of_pages = total_no_of_items / items_per_page;
-        //let page_number = 1;         
-    const items_to_skip = (pagina - 1) * items_per_page;
-    let items = todos.slice(items_to_skip, items_per_page + items_to_skip);
-    renderTodoList();
-    items.map((todo) => {                        
-        renderTodo(todo);
-    });
-
-    
-   
-   } else {
-    console.log('error 500');
-    pagina = pagina - 1;
-    const total_no_of_items = todos.length;
-    const items_per_page = 3;
-        //let no_of_pages = total_no_of_items / items_per_page;
-        //let page_number = 1;         
-    const items_to_skip = (pagina - 1) * items_per_page;
-    let items = todos.slice(items_to_skip, items_per_page + items_to_skip);
-    renderTodoList();
-    items.map((todo) => {                        
-        renderTodo(todo);
-    });
-    document.getElementById('numeroDePaginas').innerHTML = 'Pagina: ' + pagina + ' / ' + x + ' en primeros datos';
-    //desmarcado();
-   }
-
-   
-    
+        const total_no_of_items = todos.length;       
+        const items_to_skip = (pagina - 1) * limite;
+        let items = todos.slice(items_to_skip, limite + items_to_skip);
+        renderTodoList();
+        items.map((todo) => {                        
+            renderTodo(todo);
+        });
+    } else {
+        pagina = pagina - 1;
+        const items_to_skip = (pagina - 1) * limite;
+        let items = todos.slice(items_to_skip, limite + items_to_skip);
+        renderTodoList();
+        items.map((todo) => {                        
+            renderTodo(todo);
+        });
+        document.getElementById('numeroDePaginas').innerHTML = 'Pagina: ' + pagina + ' / ' + x;
+    }    
 }
 
 
 
-
-
 function siguienteP() {
+
     pagina = pagina + 1;
-
-
-
     let cantidadPaginas = todos.length / limite;
     let paginasTotales = todos.length / limite;
     let x = Math.ceil(cantidadPaginas);
 
-    console.log(paginasTotales - x);
-    let pExistente = paginasTotales - x;
-   
-    //console.log('Pagina: ' + pagina + ' / ' + x);
-
     if (pagina >= x) {
        
-        console.log('Entramos a la condicion 1 de Siguiente rata');
-        document.getElementById('numeroDePaginas').innerHTML = 'Pagina: ' + pagina + ' / ' + x + ' en primeros datos';
+        document.getElementById('numeroDePaginas').innerHTML = 'Pagina: ' + pagina + ' / ' + x;
         siguiente.classList.add('hidden')
         anterior.classList.remove('hidden')
-        
-         const items_per_page = 3;
-                
-        let page_number = 1;         
-        const items_to_skip = (pagina - 1) * items_per_page;
-        const items = todos.slice(items_to_skip, items_per_page + items_to_skip);
-        console.log(items);
+
+        const items_to_skip = (pagina - 1) * limite;
+        const items = todos.slice(items_to_skip, limite + items_to_skip);
+
         renderTodoList(); 
         items.map((todo) => {
                           
              renderTodo(todo);
             
         });
-        
-        
-
-
     } else if (pagina < x) {
-        document.getElementById('numeroDePaginas').innerHTML = 'Pagina: ' + pagina + ' / ' + x + ' en primeros datos';
-        console.log('Entramos a la condicion 2 de Siguiente');
+        document.getElementById('numeroDePaginas').innerHTML = 'Pagina: ' + pagina + ' / ' + x;
         siguiente.classList.remove('hidden')
         anterior.classList.remove('hidden')
 
-         renderTodoList();
-         const items_per_page = 3;
-                
-        let page_number = 1;         
-        const items_to_skip = (pagina - 1) * items_per_page;
-        const items = todos.slice(items_to_skip, items_per_page + items_to_skip);
-        console.log(items);
-
+        renderTodoList();    
+        const items_to_skip = (pagina - 1) * limite;
+        const items = todos.slice(items_to_skip, limite + items_to_skip);
         items.map((todo) => {                
              renderTodo(todo);
         });
-
-    }
-    
-    
+    }    
 }
 
+
 function anteriorP() {
+
     pagina = pagina - 1;
     let cantidadPaginas = todos.length / limite;
     let x = Math.ceil(cantidadPaginas);
-    document.getElementById('numeroDePaginas').innerHTML = 'Pagina: ' + pagina + ' / ' + x + ' en primeros datos';
+    document.getElementById('numeroDePaginas').innerHTML = 'Pagina: ' + pagina + ' / ' + x;
 
     if (pagina <= 1 && pagina <= x) {
         renderTodoList();
-        console.log('Entramos a la condicion 1 de anterior');
         anterior.classList.add('hidden');
         siguiente.classList.remove('hidden');
 
-        const total_no_of_items = todos.length;
-        const items_per_page = 3;
-            //let no_of_pages = total_no_of_items / items_per_page;
-            //let page_number = 1;         
-        const items_to_skip = (pagina - 1) * items_per_page;
-        const items = todos.slice(items_to_skip, items_per_page + items_to_skip);
-        console.log(items)
+        const items_to_skip = (pagina - 1) * limite;
+        const items = todos.slice(items_to_skip, limite + items_to_skip);
         items.map((todo) => {                
             renderTodo(todo);
         });
-
-        
-      
     } else if (pagina > 1 && pagina < x) {
         renderTodoList();
         anterior.classList.remove('hidden');
         siguiente.classList.remove('hidden');
-        console.log('Entramos a la condicion 2 de anterior');
-        
-        const total_no_of_items = todos.length;
-        const items_per_page = 3;
-            //let no_of_pages = total_no_of_items / items_per_page;
-            //let page_number = 1;         
-        const items_to_skip = (pagina - 1) * items_per_page;
-        const items = todos.slice(items_to_skip, items_per_page + items_to_skip);
-        console.log(items)
+       
+        const items_to_skip = (pagina - 1) * limite;
+        const items = todos.slice(items_to_skip, limite + items_to_skip);
         items.map((todo) => {                
             renderTodo(todo);
         });
-      
-        
     } else if (pagina > 1 && pagina <= x) {
         
         anterior.classList.remove('hidden');
         siguiente.classList.add('hidden');
-        console.log('Entramos a la condicion 3 de anterior');
-        renderTodoList();
-        const total_no_of_items = todos.length;
-        const items_per_page = 3;
-            //let no_of_pages = total_no_of_items / items_per_page;
-            //let page_number = 1;         
-        const items_to_skip = (pagina - 1) * items_per_page;
-        const items = todos.slice(items_to_skip, items_per_page + items_to_skip);
-        console.log(items)
+
+        renderTodoList();       
+        const items_to_skip = (pagina - 1) * limite;
+        const items = todos.slice(items_to_skip, limite + items_to_skip);
         items.map((todo) => {                
             renderTodo(todo);
         });
-               
-        
     }  
-}
-
-
-
-
-
-
-
-
-
-function despues() {
-  
-    
-    
-    
-    //console.log('Siguiente: ' + pagina);
-
-
-
-
-
-
-
-
-
-
-    // console.log('Pagina vale: ' + pagina);  
-    // let cantidadPaginas = todos.length / limite;
-    // let x = Math.round(cantidadPaginas) + 1;
-    // if (pagina <= x) {        
-        
-    //     siguiente.removeAttribute("disabled");
-    //     const total_no_of_items = todos.length;
-    //       const items_per_page = 3;
-    //       //let no_of_pages = total_no_of_items / items_per_page;
-    //       //let page_number = 1;
-         
-    //       const items_to_skip = (pagina - 1) * items_per_page;
-    //       const items = todos.slice(items_to_skip, items_per_page + items_to_skip);
-    //       console.log(items)
-          		
-	// } else {            
-	// 	siguiente.setAttribute("disabled", true);
-    //     //antes();
-        
-       
-    //     console.log('desactivado');
-	// }    
-}
-
-
-function antes() {
-    pagina = pagina - 1;
-    // //console.log('Pagina vale: ' + pagina); 
-    // let cantidadPaginas = todos.length / limite;
-    // let x = Math.round(cantidadPaginas) + 1;
-
-    // if (pagina > 1 && pagina <= x) {
-	// 	anterior.removeAttribute("disabled");//activada
-    //     console.log('esta es la pagina numero: ' + pagina);
-    //     const total_no_of_items = todos.length;
-    //       const items_per_page = 3;
-    //       //let no_of_pages = total_no_of_items / items_per_page;
-    //       //let page_number = 1;
-         
-    //       const items_to_skip = (pagina - 1) * items_per_page;
-    //       const items = todos.slice(items_to_skip, items_per_page + items_to_skip);
-    //       console.log(items)
-        
-
-	// } else {
-
-    //     anterior.setAttribute("disabled", true);
-    //     despues();          
-    //     console.log('boton anterior desactivado' + pagina);
-        
-	// }
-   
-}
-
-
-
-// anterior.addEventListener("click", retrocederPagina);
-// siguiente.addEventListener("click", avanzarPagina);
-
-
-
-
-// $todoBody.addEventListener('click', paginacion)
-// function paginacion(){
-//  //const elementos = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]; 
- 
-//  const total_no_of_items = todos.length;
-//  const items_per_page = 3;
-//  let no_of_pages = total_no_of_items / items_per_page;
-//  let page_number = 1;
- 
-//  const items_to_skip = (page_number - 1) * items_per_page;
-//  const items = todos.slice(items_to_skip, items_per_page + items_to_skip);
-//  console.log(items) 
-// }
-
-function quitarBotones() {
-    siguiente.classList.add('hidden')
-    anterior.classList.add('hidden')
 }
